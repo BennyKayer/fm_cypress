6fd0d0e46d925ea709fbfca19838e73927c8cb40
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-04-20 20:38:19 +0200

    Generating Tests

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,31 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  for (const property of properties) {
+    it(`should have a column for ${property}`, () => {
+      cy.get(`#${property}-column`);
+    });
+
+    it(`should hide the ${property} column if unchecked`, () => {
+      cy.get(`#show-${property}`).uncheck();
+      cy.get(`#${property}-column`).should('be.hidden');
+    });
+  }
+
+  restaurants.forEach((restaurant) => {
+    it(`should only display rows that match ${restaurant} when selected`, () => {
+      cy.get('@restaurant-filter').select(restaurant);
+      cy.get(`td[headers="whereToOrder-column"]`).should('contain', restaurant);
+    });
+  });
+
+  ratings.forEach((rating) => {
+    it(`should hide table rows with rating lower than ${rating}`, () => {
+      cy.get('#minimum-rating-visibility').invoke('val', `${rating}`).trigger('input');
+      cy.get('.popularity').each((el) => {
+        expect(+el.text()).to.be.gte(rating);
+      });
+    });
+  });
 });

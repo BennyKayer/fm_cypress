2945dc28e5cee82831ec198fca5bb73d73a194b7
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-05-17 18:20:23 +0200

    Intercept practice

--- cypress/integration/12-dog-facts.spec.js
+++ cypress/integration/12-dog-facts.spec.js
@@ -16,13 +16,43 @@ describe('Dog Facts', () => {
     cy.get('@emptyState');
   });
 
-  it('should make a request when the button is called', () => {});
+  it('should make a request when the button is called', () => {
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+  });
 
-  it('should adjust the amount when the select is changed', () => {});
+  it('should adjust the amount when the select is changed', () => {
+    cy.get('@amountSelect').select('9');
+    cy.get('@amountSelect').should('contain', '9 Facts');
+  });
 
-  it('should show the correct number of facts on the page', () => {});
+  it('should show the correct number of facts on the page', () => {
+    cy.get('@amountSelect').select('9');
 
-  it('should clear the facts when the "Clear" button is pressed', () => {});
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
 
-  it("should reflect the number of facts we're looking for in the title", () => {});
+    cy.get('@emptyState').should('not.exist');
+    cy.get('section#facts').should('exist');
+    cy.get('section#facts').children().should('have.length', 9);
+  });
+
+  it('should clear the facts when the "Clear" button is pressed', () => {
+    cy.get('@fetchButton').click();
+
+    cy.get('@emptyState').should('not.exist');
+    cy.get('section#facts').should('exist');
+
+    cy.get('@clearButton').click();
+
+    cy.get('@emptyState').should('exist');
+  });
+
+  it("should reflect the number of facts we're looking for in the title", () => {
+    cy.get('@amountSelect').select('3');
+    cy.get('@fetchButton').click();
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.match(/\?amount=3$/);
+    });
+  });
 });
